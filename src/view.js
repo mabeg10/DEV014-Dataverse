59dc29f705dd07c943b4d45576e554ef3bd95c5d
mbguzman.099@gmail.com <mabe@192.168.1.32>
2024-03-26 15:08:46 -0500

    primeroscambios guardados

--- src/view.js
+++ src/view.js
@@ -1,6 +1,13 @@
 export const renderItems = (data) => {
   console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
+  const elementUl = document.createElement('ul');
+  renderItems.forEach(function(data) {
+    console.log(data);
+
+  });
+  
+
   return 'example';
 };
 
